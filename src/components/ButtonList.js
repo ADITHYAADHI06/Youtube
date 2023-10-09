539afa9f4f945aee425eb46c6848e70a7c266045
ADITHYAADHI06 <adishettygu102@gmail.com>
2023-10-09 15:23:29 +0530

    first commit

---
+++ src/components/ButtonList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonList = () => {
+    return (
+        <div>ButtonLists</div>
+    )
+}
+
+export default ButtonList
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ButtonListContainer from './ButtonListContainer'
+import VideoContainer from './VideoContainer'
+
+const Main = () => {
+    return (
+        <div>
+            Main
+            <ButtonListContainer />
+            <VideoContainer />
+        </div>
+    )
+}
+
+export default Main
